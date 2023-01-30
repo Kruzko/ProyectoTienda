ad36104de5beef85c7c69fec63eb4dad2c89b8ce
Juan Jose Marin Molina <Hermanomayor2@gmail.com>
2023-01-30 14:27:11 +0100

    Commit message

---
+++ pTienda/install.sh
@@ -0,0 +1,10 @@
+
+echo "tareas por etapas del pipeline jenkinsfile"
+
+echo "instalando la tienda app pc componentes"
+echo "descargar repositorio apptienda"
+echo "actualizar versiones de los dockerfiles en dockerhub"
+echo "realiza el build del proyecto "
+echo "fases de testeo"
+echo "matar los contenedores en ejecucion"
+echo "levantar nuevos contenedores actualizados"
