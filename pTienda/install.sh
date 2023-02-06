3e89820aa3d2c72107221b5dbfff9db62c86dba9
Juan Jose Marin Molina <Hermanomayor2@gmail.com>
2023-02-06 13:59:53 +0100

    docker compose y demas

--- pTienda/install.sh
+++ pTienda/install.sh
@@ -1,10 +1,15 @@
 
-echo "tareas por etapas del pipeline jenkinsfile"
+# echo "tareas por etapas del pipeline jenkinsfile"
 
-echo "instalando la tienda app pc componentes"
-echo "descargar repositorio apptienda"
-echo "actualizar versiones de los dockerfiles en dockerhub"
-echo "realiza el build del proyecto "
-echo "fases de testeo"
-echo "matar los contenedores en ejecucion"
-echo "levantar nuevos contenedores actualizados"
+# echo "instalando la tienda app pc componentes"
+# echo "descargar repositorio apptienda"
+# echo "actualizar versiones de los dockerfiles en dockerhub"
+# echo "realiza el build del proyecto "
+# echo "fases de testeo"
+# echo "matar los contenedores en ejecucion"
+# echo "levantar nuevos contenedores actualizados"
+
+echo "desplegando la app de pTienda"
+
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
